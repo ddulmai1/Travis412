9adcd52f30c011c68433cb00932888694dd881ae
Dylan Dulmaine <ddulmai1@students.towson.edu>
2026-03-03 16:24:09 -0500

    Tested the Associative property of all SimpleCalculator functions

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -13,9 +13,21 @@ public class SimpleCalculatorTest {
 	}
 
     @Test
+    public void testAddAssociative() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.add(2, 1), calc.add(1, 2));
+    }
+
+    @Test
     public void testSubtract() {
         SimpleCalculator calc = new SimpleCalculator();
         assertEquals(2, calc.subtract(7,5));
+    }
+
+    @Test
+    public void testSubtractAssociative() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertNotEquals(calc.subtract(2, 1), calc.subtract(1, 2));
     }
 
     @Test
@@ -25,9 +37,21 @@ public class SimpleCalculatorTest {
     }
 
     @Test
+    public void testMultiplyAssociative() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.multiply(2, 1), calc.multiply(1, 2));
+    }
+
+    @Test
     public void testDivide() {
         SimpleCalculator calc = new SimpleCalculator();
         assertEquals(2, calc.divide(14, 7));
+    }
+
+    @Test
+    public void testDivideAssociative() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertNotEquals(calc.divide(2, 1), calc.divide(1, 2));
     }
 
     @Test(expected=ArithmeticException.class)
