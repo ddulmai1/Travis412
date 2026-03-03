c696571ab49c59fd87015dec48d740e55cbad050
Dylan Dulmaine <ddulmai1@students.towson.edu>
2026-03-03 16:13:14 -0500

    Implemented the divide functionality

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -6,4 +6,5 @@ public class SimpleCalculator {
 	}
     public int subtract(int a, int b) {return a - b;}
     public int multiply(int a, int b) {return a * b;}
+    public int divide(int a, int b) {return a / b;}
 }
